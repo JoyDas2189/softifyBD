b61efac02a92d2687291ef64544d986d94be865f
Joy Das <dasjoy365d@gmail.com>
2024-07-14 00:21:52 +0600

    callback function added

---
+++ jsCallback.js
@@ -0,0 +1,37 @@
+/* 
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+function myCalculator(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+let result = myCalculator(10, 5);
+myDisplayer(result);
+ */
+
+/* 
+function myDisplayer (some) {
+    document.getElementById('demo').innerHTML=some;
+}
+function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    myDisplayer(sum);
+}
+myCalculator(10, 10); 
+*/
+
+// Using Callback function
+
+function displayer (some) {
+    document.getElementById('demo').innerHTML = some;
+}
+
+function calculator (num1, num2, callback) {
+    let mult = num1 * num2;
+    callback(mult);
+}
+
+calculator(10, 7, displayer);
